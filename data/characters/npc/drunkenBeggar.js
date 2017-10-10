acf308bd7c5fa779c68c80f88459f8aa7ea9c353
bavaz1 <kismadar12@gmail.com>
2017-10-10 13:04:55 +0200

    Kocsmai emberek, és itemeik

---
+++ data/characters/npc/drunkenBeggar.js
@@ -0,0 +1,11 @@
+'use strict';
+//részeg koldus
+module.exports = {
+    name: 'Részeg koldus',
+    hp: 6,
+    str: 2,
+    int: 1,
+    agi: 1,
+    items: 'Alcohol',
+    aggro: false
+};
---
+++ data/characters/npc/innkeeper.js
@@ -0,0 +1,11 @@
+'use strict';
+//kocsmáros
+module.exports = {
+    name: 'Kocsmáros',
+    hp: 16,
+    str: 5,
+    int: 3,
+    agi: 4,
+    items: 'Coffee',
+    aggro: false
+};
