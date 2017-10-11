595710659d19f30f424da863e2c7f97665930381
Stickz <vastag.balint@gmail.com>
2017-10-11 11:09:49 +0200

    fixed for buildMap function

--- data/game-area/index.js
+++ data/game-area/index.js
@@ -1,7 +1,9 @@
 'use strict';
 const road = require('./game-field/road');
-let matrix= [];
 
+
+module.exports = function buildMap(){
+    let matrix= [];
     for(let i=0 ; i<10 ; i++){
         matrix[i] = [];
         for(let j=0 ; j<10; j++ ){
@@ -9,5 +11,5 @@ let matrix= [];
             matrix[i][j] = (road);
         }
     }
-
-console.log(matrix);
+    return matrix[i][j];
+}
