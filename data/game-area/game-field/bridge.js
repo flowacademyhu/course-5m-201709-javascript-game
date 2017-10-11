1f83b51d4c4e522252e65ff2ceded42aa63a5112
Stickz <vastag.balint@gmail.com>
2017-10-11 11:05:53 +0200

    fixed game areas hash and made matrix generator

--- data/game-area/game-field/bridge.js
+++ data/game-area/game-field/bridge.js
@@ -1,8 +1,8 @@
 'use strict'
 
 module.exports = {
-    desc = 'Egy omladozó kőhídon állsz, két oldalt egy vad folyó vize folyik.',
-    shortDesc = 'látsz egy hidat',
-    npc = [],
-    accessible = true
+    desc: 'Egy omladozó kőhídon állsz, két oldalt egy vad folyó vize folyik.',
+    shortDesc: 'látsz egy hidat',
+    npc: [],
+    accessible: true
 }
---
+++ data/game-area/index.js
@@ -0,0 +1,13 @@
+'use strict';
+const road = require('./game-field/road');
+let matrix= [];
+
+    for(let i=0 ; i<10 ; i++){
+        matrix[i] = [];
+        for(let j=0 ; j<10; j++ ){
+            matrix[i][j];
+            matrix[i][j] = (road);
+        }
+    }
+
+console.log(matrix);
