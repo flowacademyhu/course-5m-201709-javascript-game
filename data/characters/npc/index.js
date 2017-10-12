6fe96ff65cae9a944d7f0505e8ca43673789164b
Vastag Bálint <32384589+Stickzg@users.noreply.github.com>
2017-10-12 10:57:26 +0200

    Update index.js

--- data/characters/npc/index.js
+++ data/characters/npc/index.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 let npcs = fs.readdirSync(files)
     .filter(file => file !== 'index.js')
     .map(file => require('./' + file))
-    .map(obj => obj.name)
 
-console.log(npcs)
+console.log(npcs);
 module.exports = npcs;
