86c069ca5c4fde283f7cbb1a1862efe799ce09fc
bavaz1 <kismadar12@gmail.com>
2017-10-13 16:58:41 +0200

    Drunken wizardos gamefield

--- data/game-area/game-field/index.js
+++ data/game-area/game-field/index.js
@@ -5,6 +5,7 @@ const grassfield = require('./grassfield');
 const river = require('./river');
 const woods = require('./woods');
 const castle = require('./castle');
+const roadDrunkenWizard = require('./road-drunken-wizard');
 
 module.exports = {
   0: woods,       1: woods,  2: woods,  3: woods,  4: woods,       5: woods,       6: woods,       7: road,        8:  grassfield, 9:  grassfield,
@@ -16,5 +17,5 @@ module.exports = {
   60: woods,      61: woods, 62: woods, 63: road,  64: road,       65: road,       66: road,       67: road,       68: grassfield, 69: grassfield,
   70: woods,      71: woods, 72: woods, 73: road,  74: grassfield, 75: grassfield, 76: grassfield, 77: grassfield, 78: grassfield, 79: grassfield,
   80: grassfield, 81: woods, 82: woods, 83: road,  84: grassfield, 85: grassfield, 86: grassfield, 87: grassfield, 88: grassfield, 89: grassfield,
-  90: grassfield, 91: road,  92: road,  93: road,  94: grassfield, 95: grassfield, 96: grassfield, 97: grassfield, 98: grassfield, 99: grassfield 
+  90: grassfield, 91: road,  92: road,  93: roadDrunkenWizard,  94: grassfield, 95: grassfield, 96: grassfield, 97: grassfield, 98: grassfield, 99: grassfield 
 };
---
+++ data/game-area/game-field/road-drunken-wizard.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const drunkenWizard = require('../../characters/npc/drunken-wizard');
+
+module.exports = {
+  desc: 'Egy poros úton állsz. Varázslós.',
+  shortDesc: 'látsz egy utat. Varázslós.',
+  npc: [drunkenWizard],
+  accessible: true
+};
