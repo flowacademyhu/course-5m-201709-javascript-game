f7c15a304039128514418c8dee61642b069a4225
vitriolfox <vitriol.fox@gmail.com>
2017-10-13 10:03:56 +0200

    grassfield and woods gamefields

---
+++ data/game-area/game-field/grassfield.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+  desc: 'Csendesen ringó fűtenger közepén állsz.',
+  shortDesc: 'zöldellő füvet látsz',
+  npc: [],
+  accessible: true
+};
---
+++ data/game-area/game-field/woods.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+  desc: 'Árnyas erdőben állsz, fölötted a zöld lobkoronák leple.',
+  shortDesc: 'árnyékos erdőt látsz',
+  npc: [],
+  accessible: true
+};
