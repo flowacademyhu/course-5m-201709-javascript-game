5b91505ef26cefd836d440b7f2ee44eac11f896d
vitriolfox <vitriol.fox@gmail.com>
2017-10-16 10:03:17 +0200

    modulised functions from game file

--- data/game-area/index.js
+++ data/game-area/index.js
@@ -1,13 +1,20 @@
 'use strict';
-const road = require('./game-field/road');
 
-module.exports = () => {
+const matrixCurrentPosition = require('./matrix-current-position');
+const position = require('./starter-position');
+const gameArea = require('./game-field/index');
+
+module.exports = function map () {
   let matrix = [];
+  let n = 0;
+
   for (let i = 0; i < 10; i++) {
     matrix[i] = [];
     for (let j = 0; j < 10; j++) {
-      matrix[i][j] = (road);
+      matrix[i][j] = n;
+      n++;
     }
   }
+  matrixCurrentPosition(position[1], position[0], matrix, gameArea);
   return matrix;
 };
