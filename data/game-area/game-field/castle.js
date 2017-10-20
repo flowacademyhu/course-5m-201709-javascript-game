111e7ce7316c5a76fc85089ea6fb7dabb545611a
vitriolfox <vitriol.fox@gmail.com>
2017-10-20 10:53:37 +0200

    key and princess added to game

--- data/game-area/game-field/castle.js
+++ data/game-area/game-field/castle.js
@@ -3,11 +3,11 @@
 const dragon = require('../../characters/npc/dragon');
 
 module.exports = {
-  desc: 'A várban csücsül a' + '\x1b[31m' + 'sárkány' + '\x1b[0m' +
+  desc: 'A várban csücsül a ' + '\x1b[31m' + 'sárkány' + '\x1b[0m' +
   ' üsd meg hát mielőtt ő süt meg téged. A sárkánynál a kircsilány, ha megszerzed nyertél ám!',
   deadNpcDesc: null,
   shortDesc: 'az öreg vártorony magasodik kapuja zárva, míg a kulcsot meg nem szerzed hozzá.',
   npc: dragon,
   item: null,
-  accessible: true
+  accessible: false
 };
