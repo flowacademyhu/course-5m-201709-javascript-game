7ba18823105e5b9e84b8d73da7220c0495e6ccdd
bavaz1 <kismadar12@gmail.com>
2017-10-20 16:29:52 +0200

    filename changes gergo wip

--- app/controllers/actions/place.js
+++ app/controllers/actions/place.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const matrixCurrentPosition = require('../../data/game-area/matrix-current-position');
-const gameArea = require('../../data/game-area/game-field/index');
-const position = require('../../data/game-area/starter-position');
+const matrixCurrentPosition = require('../../models/game-area/matrix-current-position');
+const gameArea = require('../../models/game-area/game-field/index');
+const position = require('../../models/game-area/starter-position');
 
 module.exports = function (matrix) {
   matrixCurrentPosition(position[1], position[0], matrix, gameArea);
